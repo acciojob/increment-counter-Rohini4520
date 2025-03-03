2ba70b2cad8bc2852b34826611f8fd8a91faec38
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-03-03 14:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count = 0;
+
+document.getelementBYId("incrementBtn").addEventListner("click" function(){
+
+	alert("current value: " count);
+	count++;
+	 document.getElementById("counter").textContent = count;
+});
